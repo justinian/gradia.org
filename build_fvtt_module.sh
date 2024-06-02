962ea1164c8c082600fa8925656d25468e11569c
Justin C. Miller <justin@devjustinian.com>
2024-06-02 12:10:57 -0700

    Module: Update info and add thumbnail

--- build_fvtt_module.sh
+++ build_fvtt_module.sh
@@ -11,10 +11,11 @@ mkdir -p out/packs/
 
 python3 ./id_replacer.py public/fvtt
 sed "s/__version__/${VERSION}/g" assets/fvtt/module.json > out/module.json
+cp -r assets/fvtt/ui out/ui
 
 env bun x @foundryvtt/foundryvtt-cli package pack \
     --id gradia-org --type Module -n info \
     --in public/fvtt --out out/packs/ -v -r
 
 rm out/gradia-org.zip
-cd out; zip -v -r gradia-org.zip module.json packs
+cd out; zip -v -r gradia-org.zip module.json packs ui
